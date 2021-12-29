32b14c83898370b3ff43c6e09d06b1e0b6743e6d
Zechariah Adebayo <88634669+Zedtek-me@users.noreply.github.com>
2021-12-29 22:16:00 +0100

    base_dir palaba

--- Api/settings.py
+++ Api/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +126,4 @@ STATIC_DIRS= [os.path.join(BASE_DIR, 'api_app/static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
